188ba26a3afcff79eecf0e788ddf3fa7bf1b2c35
José Luis Arroyo Núñez <51970076+DecodeLuis001@users.noreply.github.com>
2023-12-12 14:14:37 -0600

    Create Euler.py
    El método de Euler en métodos numéricos es una técnica utilizada para resolver ecuaciones diferenciales ordinarias (EDO) mediante la aproximación de la solución a través de pasos sucesivos. 
    
    Este método se basa en la idea de aproximar la pendiente de la solución en un punto dado y utilizar esta aproximación para predecir el valor de la solución en el siguiente punto. 
    
    El método de Euler es sencillo de implementar, pero puede introducir errores significativos, especialmente en EDO con tasas de cambio rápidas o en intervalos de paso grandes. 
    
    A pesar de sus limitaciones, el método de Euler es un punto de partida común para comprender y desarrollar métodos más sofisticados de resolución de EDO en el contexto de métodos numéricos.

---
+++ Euler.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def euler(f, y0, t0, tn, h):
+    """
+    Método de Euler para resolver ecuaciones diferenciales ordinarias (EDO).
+
+    Parameters:
+    - f: Función que define la EDO dy/dt = f(t, y).
+    - y0: Condición inicial.
+    - t0: Tiempo inicial.
+    - tn: Tiempo final.
+    - h: Tamaño del paso.
+
+    Returns:
+    - t_vals: Lista de tiempos.
+    - y_vals: Lista de soluciones y en cada tiempo t.
+    """
+    t_vals = np.arange(t0, tn + h, h)
+    y_vals = [y0]
+
+    for t in t_vals[:-1]:
+        y_next = y_vals[-1] + h * f(t, y_vals[-1])
+        y_vals.append(y_next)
+
+    return t_vals, y_vals
+
+# Ejemplo de uso:
+def funcion_edo(t, y):
+    """
+    Define la EDO dy/dt = -y.
+
+    Parameters:
+    - t: Tiempo.
+    - y: Valor de la función y en el tiempo t.
+
+    Returns:
+    - Derivada de y en el tiempo t según la EDO.
+    """
+    return -y
+
+# Condiciones iniciales y parámetros
+y0 = 1.0
+t0 = 0.0
+tn = 5.0
+h = 0.1
+
+# Aplicar el método de Euler
+t_resultado, y_resultado = euler(funcion_edo, y0, t0, tn, h)
+
+# Graficar los resultados
+plt.plot(t_resultado, y_resultado, label='Solución numérica')
+plt.xlabel('Tiempo (t)')
+plt.ylabel('y(t)')
+plt.title('Método de Euler para dy/dt = -y')
+plt.legend()
+plt.show()
